5ed2717ce766104666b2eae36176718d7bfb5d07
Mohamad Hamsho <hamsho1030@gmail.com>
2026-03-02 16:01:10 -0500

    Added a timer for the modal in order for to be able to submit emails and close automatically

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const modal = document.getElementById('modal');
 const modalCloseBtn = document.getElementById('modal-close-btn');
-
-
-
+const modalForm = document.getElementById('modal-form')
+const modalTitle = document.getElementById('modal-title')
+const modalText = document.getElementById('modal-text')
+const modalEmail = document.getElementById('modal-email')
+const modalSubmitBtn = document.getElementById("modal-submit-btn")
+//modal function and close function
 setTimeout(() => {
     modal.style.display = 'inline';
 }, 1500);
@@ -13,3 +16,21 @@ setTimeout(() => {
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none';
 })
+
+//modal submit function
+modalForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('submitted')
+    setTimeout(() => {
+        modalTitle.textContent = 'We got your email great !'
+        modalText.textContent = 'We will be sending you the best deal make sure to check your email.'
+        modalEmail.style.display = 'none'
+        modalSubmitBtn.style.display = 'none'
+    }, 1000);
+
+    setTimeout(() => {
+        modal.style.display='none'
+    }, 3000);
+})
+
+
